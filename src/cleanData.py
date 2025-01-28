92ae503d9c7ded89258f32d5ef36334316d6845b
Plaza <mplaza0627@gmail.com>
2025-01-27 21:16:51 -0500

    Trying to include Segment changes ... Todo, trouble shoot yearly segment data to see what the issue is ...

--- src/cleanData.py
+++ src/cleanData.py
@@ -32,7 +32,7 @@ def getInfo(df):
 
 def cleanExcel(fileName):
     df = pd.read_excel(fileName, header=None)
-
+    
     # Saving the Stock Name
     Stock_Name = df.iloc[0, 0]  
     Stock_Name = Stock_Name.replace(" ", "_")
@@ -75,6 +75,54 @@ def cleanExcel(fileName):
     return transposed_df
 
 
+def cleanExcelSegments(fileName):
+    df = pd.read_excel(fileName, header=None)
+    
+    # Saving the Stock Name
+    #Stock_Name = df.iloc[0, 0]  
+    #Stock_Name = Stock_Name.replace(" ", "_")
+
+    #-------------------------
+    # Step 1 Extract everything from row 6 onward RAW
+    df_from_row_6 = df.iloc[4:].copy()  # Rows start from 0, so row 6 is index 5
+
+    # General Clean up of the Excel
+    df_from_row_6.iloc[0, 0] = "Parameter"
+    for col in df_from_row_6.columns:
+        if pd.isna(df_from_row_6.iloc[0][col]):
+            df_from_row_6 = df_from_row_6.drop(columns=[col])
+    df_from_row_6.columns = df_from_row_6.iloc[0, :]
+
+    df_from_row_6 = df_from_row_6.reset_index(drop=True)
+    df_from_row_6 = df_from_row_6.drop(0).reset_index(drop=True)
+
+    # Saving the initial Names of the Variables
+    parameterToName = defaultdict(list)
+    parameterToName['_'].append('Revenue')
+    
+    for i in df_from_row_6.index:
+        parameterToName[df_from_row_6.iloc[i, 1]].append(df_from_row_6.iloc[i, 0])
+
+    #df_from_row_6 = df_from_row_6.drop('Parameter', axis=1)
+    
+    # Dropping NaN Parameters because they should not have any data
+    df_from_row_6 = df_from_row_6.dropna(subset=['Parameter'])
+
+    df_from_row_6 = df_from_row_6.drop_duplicates(subset=['Parameter'])
+
+
+    #-------------------------
+    # Step 2 Transposing the Dataframe
+    transposed_df = df_from_row_6.set_index("Parameter").T.copy()
+    # Cleaning up the Date column and the Index
+    transposed_df['Date']=transposed_df.index.astype(str)
+    transposed_df = transposed_df.reset_index(drop=True)
+
+    # Reordering the dataframe
+    cols = ["Date"] + [col for col in transposed_df.columns if col != "Date"]
+    transposed_df = transposed_df[cols]
+
+    return transposed_df
 
 def cleanFileName(fileName):
   # File Name Clean Up and Adding _V2
@@ -121,20 +169,25 @@ for root, dirs, files in os.walk(raw_data_folder):
 
         else:
             print(f"No matching keyword found in file: {file}")
-
+i = 0
 for segment, quarter, yearly in tqdm(zip(segments_files, quarter_files, yearly_files), desc="Cleaning Files", total=len(quarter_files)):
     # Complete Segment later
     # print("quarter len: ", len(quarter_files))
     # print("zip len: ", len(list(zip(segments_files, quarter_files, yearly_files))))
     # print("yearly len: ", len(yearly_files))
-
+    i += 1
+    if i == 1: continue
+    #quarterDF = cleanExcel(quarter)
+    #yearlyDF = cleanExcel(yearly)
+    print(segment)
+    segmentDF = cleanExcelSegments(segment)
     
-    quarterDF = cleanExcel(quarter)
-    yearlyDF = cleanExcel(yearly)
-
-    print(cleanFileName(quarter))
-    print(cleanFileName(yearly))
+    #print(cleanFileName(quarter))
+    #print(cleanFileName(yearly))
+    print(cleanFileName(segment))
+    print(segmentDF)
+    if i == 2: break
     #print(quarterDF)
     #print(yearlyDF)
-    quarterDF.to_csv(cleanFileName(quarter), index=False)
-    yearlyDF.to_csv(cleanFileName(yearly), index=False)
+    #quarterDF.to_csv(cleanFileName(quarter), index=False)
+    #yearlyDF.to_csv(cleanFileName(yearly), index=False)
